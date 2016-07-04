3465666946c46899f2922323f0a6636a8d0523a0
Knut Perseke <knut.perseke@gmail.com>
2016-07-04 12:25:46 +0200

    release

--- js/components/infobox.jsx
+++ js/components/infobox.jsx
@@ -12,15 +12,17 @@ var InfoboxComponent = React.createClass({
 
     render: function() {
         var facts = this.props.facts ? this.props.facts : [];
+        var hasfacts = this.props.facts.length > 0 ? true : false
+
         return (
             <div className="infobox">
                 <h2 className="infobox__headline">Infobox</h2>
-                <p className="infobox__text">
-                    {this.props.text}
+                <p className="infobox__text" dangerouslySetInnerHTML={{__html:this.props.text}}>
                 </p>
+                {hasfacts ? <h3>Facts</h3> : null}
                 <ul className="infobox__list">
                     {facts.map(function(data, i, list) {
-                        return (<li key={i}>{data}</li>)
+                        return (<li key={i} dangerouslySetInnerHTML={{__html:data}}></li>)
                     })}
                 </ul>
             </div>
